711384fdd9216a4ad46a6efe42a073d49aa10a1e
renatalantos <renatalantos@gmail.com>
2021-07-15 12:27:42 +0000

    Partly achieved validating user input for setting size of the board by adding while loop with multiple conditions and raising ValueError

--- battleship.py
+++ battleship.py
@@ -6,14 +6,20 @@ from random import randint
 board = []
 
 board_size = int(input('Enter number 4, 6, 8, 10 to set size of the board:\n'))
-if board_size == 2*2:
+BOARD_SIZE_FACTOR = board_size / 2  #needed a constant in order to be able to create a while loop
+# Validate user input
+while BOARD_SIZE_FACTOR < 2 or BOARD_SIZE_FACTOR > 5 or board_size % 2 != 0: 
+    raise ValueError('Please enter number 4, 6, 8 or 10. \n')
+    
+if BOARD_SIZE_FACTOR == 2:
     number_of_ships = 1
-elif board_size == 2*3:
+elif BOARD_SIZE_FACTOR == 3:
     number_of_ships = 3
-elif board_size == 2*4:
+elif BOARD_SIZE_FACTOR == 4:
     number_of_ships = 4
-else: 
+else:
     number_of_ships = 5
+
 
 print(f'Number of ships: {number_of_ships}\n')
 
