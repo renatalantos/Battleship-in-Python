cbb20c9569b4d0f5083b447981c3a9bf863275d2
renatalantos <renatalantos@gmail.com>
2021-07-15 18:10:45 +0000

    Create test.py file to set condistions for board size in while loop. No success so far

--- test.py
+++ test.py
@@ -3,11 +3,16 @@ from random import randint
 
 # Generate board, allowing it to be different sizes, using a for loop
 
-board = []
-
-board_size = int(input('Enter number 4, 6, 8, 10 to set size of the board:\n'))
 
 # Validate user input
+board_size = int(input('Enter number 4, 6, 8, 10 to set size of the board:\n'))
+board = []
+
+
+def generate_board():
+    for i in range(board_size):
+        board.append('O')
+        print('O')
 
 
 def set_board_size():
@@ -16,30 +21,31 @@ def set_board_size():
     also determine the number of ships and the number of steps allowed for the game.
     The while loop will run until the user enters a valid input.
     """
+    
+    if board_size != 4 and board_size !=6 and board_size !=8 and board_size != 10:
+        int(input('Wrong input. Please enter number 4, 6, 8, 10. \n'))
+        if board_size == 4: 
+            number_of_ships = 1
+            print(f'Number of ships in game: {number_of_ships}\n')
+        elif board_size == 6:
+            number_of_ships = 3
+            print(f'Number of ships in game: {number_of_ships}\n')
+        elif board_size == 8:
+            number_of_ships = 4
+            print(f'Number of ships in game: {number_of_ships}\n')
+        elif board_size == 10:
+            number_of_ships = 5
+            print(f'Number of ships in game: {number_of_ships}\n') 
+        generate_board()        
+    
+#print(f'Number of ships in game: {number_of_ships}\n')
 
-    while board_size < 4 or board_size > 10 or board_size % 2 != 0: 
-
-        print('Please enter number 4, 6, 8 or 10. \n')
-        break
-
-    if board_size == 4:
-        number_of_ships = 1
-        print(f'Number of ships in game: {number_of_ships}\n')
-    elif board_size == 6:
-        number_of_ships = 3
-        print(f'Number of ships in game: {number_of_ships}\n')
-    elif board_size == 8:
-        number_of_ships = 4
-        print(f'Number of ships in game: {number_of_ships}\n')
-    elif board_size == 10:
-        number_of_ships = 5
-        print(f'Number of ships in game: {number_of_ships}\n')
-
-
-#  print(f'Number of ships in game: {number_of_ships}\n')
-
-for board_elements in range(board_size):
-    board.append('O')
-    print('O')  
 
 set_board_size()
+
+
+
+
+
+
+
