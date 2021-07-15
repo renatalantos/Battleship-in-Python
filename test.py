c541fe332e807aa164e2d9983a6ec886f3873255
renatalantos <renatalantos@gmail.com>
2021-07-15 20:35:26 +0000

    Try to add try except blocks to code based on Love Sandwiches walkthrough project in order to validate user input for size of board

--- test.py
+++ test.py
@@ -8,6 +8,22 @@ from random import randint
 board_size = int(input('Enter number 4, 6, 8, 10 to set size of the board:\n'))
 board = []
 
+def set_board_size():
+    """
+    Ask for user input to set the size of the board, which will
+    also determine the number of ships and the number of steps allowed for the game.
+    The while loop will run until the user enters a valid input.
+    """
+    while True:
+        print('Enter number 4, 6, 8, 10 to set size of the board:\n')
+        board_size = int(input('Enter your number here:\n'))
+        if validate_board_size():
+            print(f'Ok, your board size is {board_size} x {board_size}')
+            generate_board() 
+            break
+    return board_size
+    
+
 
 def generate_board():
     for i in range(board_size):
@@ -15,31 +31,19 @@ def generate_board():
         print('O')
 
 
-def set_board_size():
+def validate_board_size():
     """
-    Ask for user input to set the size of the board, which will
-    also determine the number of ships and the number of steps allowed for the game.
-    The while loop will run until the user enters a valid input.
+    Inside the try, user input will be converted from string value to integers.
+    A ValueError is raised if a string cannot be converted to an integer,
+    or if the integer is outside the given range.
     """
-    
-    if board_size != 4 and board_size !=6 and board_size !=8 and board_size != 10:
-        int(input('Wrong input. Please enter number 4, 6, 8, 10. \n'))
-        if board_size == 4: 
-            number_of_ships = 1
-            print(f'Number of ships in game: {number_of_ships}\n')
-        elif board_size == 6:
-            number_of_ships = 3
-            print(f'Number of ships in game: {number_of_ships}\n')
-        elif board_size == 8:
-            number_of_ships = 4
-            print(f'Number of ships in game: {number_of_ships}\n')
-        elif board_size == 10:
-            number_of_ships = 5
-            print(f'Number of ships in game: {number_of_ships}\n') 
-        generate_board()        
-    
-#print(f'Number of ships in game: {number_of_ships}\n')
-
+    try:
+        if board_size != 4 and board_size !=6 and board_size !=8 and board_size != 10:
+            raise ValueError(int(input('Wrong input. Please enter number 4, 6, 8, 10. \n')))
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True           
 
 set_board_size()
 
