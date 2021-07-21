b2f18e55c53a5c082ebf29360ff8035bb80fd2b6
renatalantos <renatalantos@gmail.com>
2021-07-21 19:51:16 +0000

    Add display welcome message function and brainstorm for ideas to make game more interesting

--- hangman.py
+++ hangman.py
@@ -1,6 +1,13 @@
 
 import random
-word_list = ['hello', 'Mamma', 'joint']
+word_list = ['Two and a Half Men', 'Friends', 'The Big Bang Theory', 'Dallas']
+    
+def display_welcome_message():
+    print('Hello! Welcome! Play my hangman game!\n')
+    name = input('Please enter your name: \n').upper()
+    print(f'Thank you, {name}! Hope you are ready to play!')
+ 
+    
 def create_random_word():
 
     word = random.choice(word_list)
@@ -15,45 +22,50 @@ def play_game(word):
     guesses_left = 6
 
     while not guess_made and guesses_left > 0: 
-        guess = input('Please guess a letter: \n').upper()
-        if len(guess) == 1 and guess.isalpha():
-            if guess in letters_guessed:
-                print(f'You have guessed the guess {guess} already!\n')  
-            elif guess not in word:
-                print(f'{guess} is not in the word!\n')
+        guess_letter = input('Please guess a letter: \n').upper()
+        if len(guess_letter) == 1 and guess_letter.isalpha():
+            if guess_letter in letters_guessed:
+                print(f'You have guessed {guess_letter} already!\n')
+                already_guessed = str(letters_guessed).strip('[]')
+                print(f'You have guessed the following letters so far: {already_guessed}\n')  
+            elif guess_letter not in word:
+                print(f'Sorry, {guess_letter} is not the word!\n')
                 guesses_left -= 1
-                letters_guessed.append(guess)
+                letters_guessed.append(guess_letter)
            
             else:
-                print(f'Great! {guess} is in the word!')
+                print(f'Great! {guess_letter} is in the word!')
                 word_as_list = list(game_lines)
-                letters_guessed.append(guess)
-                indexes = [i for i, letter in enumerate(word) if letter == guess]
+                letters_guessed.append(guess_letter)
+                indexes = [i for i, letter in enumerate(word) if letter == guess_letter]
                 for index in indexes:
-                    word_as_list[index] = guess
+                    word_as_list[index] = guess_letter
                 game_lines = "".join(word_as_list)
                 if '_' not in game_lines:
                     guess_made = True
-        elif len(guess) == len(word) and guess.isalpha():
-            if guess in words_guessed:
-                print('You already guessed that word!\n')
-            elif guess != word:
-                print(f'{guess} is not the word!\n')
+        elif len(guess_letter) == len(word) and guess_letter.isalpha():
+            if guess_letter in words_guessed:
+                print(f'You have guessed the word already!\n')
+            elif guess_letter != word:
+                print(f'Sorry, {guess_letter} is not the word!\n')
                 guesses_left -= 1
-                words_guessed.append(guess)
+                words_guessed.append(guess_letter)
                 guess_made = True
                 game_lines = word
-        else: 
-            print('Not a valid guess.\n')
+        else:
+            if guess_letter.isalpha() is False: 
+                print('You need to enter a letter. This was a different character. \n')
+            elif len(guess_letter) > 1 and guess_letter.isalpha:
+                print('You entered more than one letter. \n') 
         print(game_lines)
     if guess_made:
-        print('You guessed the word!')
+        print('Congratulations! You guessed the word!')
     else: 
-        print('sorry')                            
-
+        print('Oh, no! The game is over! Here comes the hangman!')                            
 
 
 def main():
+    display_welcome_message()
     word = create_random_word()
     play_game(word)
 
